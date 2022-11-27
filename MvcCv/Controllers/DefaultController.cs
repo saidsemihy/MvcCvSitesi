b7d230869be9a92d49aee6bd6992aae50a3acd77
Said Semih Yalcin <saidsemihy@gmail.com>
2022-11-27 21:12:36 +0300

    MvcCV

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DvCvEntities db = new DvCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult SosyalMedya()
+        {
+            var sosyalmedya = db.TblSosyalMedya.Where(x=>x.Durum==true).ToList();
+            return PartialView(sosyalmedya);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler=db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TblEgitimlerim.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var egitimler=db.TblYeteneklerim.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifika = db.TblSertifikalarim.ToList();
+            return PartialView(sertifika);
+        }
+        [HttpGet]
+        public PartialViewResult Iletisim()
+        {
+            
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Iletisim(Tbliletisim t)
+        {
+            t.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Tbliletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+
+    }
+}
---
+++ MvcCv/Repositories/DeneyimRepository.cs
@@ -0,0 +1,15 @@
+﻿using MvcCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCv.Repositories
+{
+    public class DeneyimRepository:GenericRepository<TblDeneyimlerim>
+    {
+        DvCvEntities db=new DvCvEntities();
+
+
+    }
+}
